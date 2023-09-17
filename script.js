fb9c915036173db3e87dd9a1e8019dbbc143c0c9
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2023-09-17 10:17:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+var modal = document.getElementById("modal");
+var openModalBtn = document.getElementById("openModal");
+var closeModalBtn = document.querySelector(".close-modal");
+
+openModalBtn.addEventListener("click", function() {
+  modal.style.display = "block";
+});
+
+closeModalBtn.addEventListener("click", function() {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+});
